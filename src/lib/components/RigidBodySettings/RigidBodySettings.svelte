901ba94d00cf319b3137c4d759dc4a26e1a7f9bf
juliano <julianoh1909@gmail.com>
2023-12-27 14:14:20 -0300

    add a max distance to which orbs can dance around the center

--- src/lib/components/RigidBodySettings/RigidBodySettings.svelte
+++ src/lib/components/RigidBodySettings/RigidBodySettings.svelte
@@ -8,30 +8,63 @@
 	import { T, useTask } from '@threlte/core';
 	import { Attractor, Collider, type GravityType, useRigidBody } from '@threlte/rapier';
 	import type { Config } from '$lib/types/config';
+	import { onMount } from 'svelte';
 
 	export let config: Config;
 	export let hasAttractor = false;
 	export let type: GravityType = 'static';
 
 	const accelerationRate = 2; // Adjust the rate of acceleration
-	const orbitalDecreaseRate = 1000; // Rate at which the distance to the center decreases
 
 	const rigidBody = useRigidBody();
+	const maxDistance = 50; // Set your maximum distance
+	const center = new Vector3(0, 0, 0); // Assuming center is at (0, 0, 0)
+	let it = 0;
 
 	useTask((delta) => {
+		// Increase velocity for the orbiting rigid bodies
+		if (it < 10) {
+			if (rigidBody?.userData?.id === 1) {
+				rigidBody.setTranslation(new Vector3(0, 50, 0), true);
+				it++;
+			}
+			if (rigidBody?.userData?.id === 2) {
+				rigidBody.setTranslation(new Vector3(0, -50, 0), true);
+				it++;
+			}
+		}
+
 		if (rigidBody?.userData?.id === 1 || rigidBody?.userData?.id === 2) {
-			updateOrbitalVelocity(rigidBody, accelerationRate);
+			updateOrbitalVelocity(rigidBody, accelerationRate, maxDistance, center);
 		}
 	});
 
-	function updateOrbitalVelocity(rigidB: typeof rigidBody, rate: number) {
-		const { x, y, z } = rigidB?.translation();
+	function updateOrbitalVelocity(
+		rigidB: typeof rigidBody,
+		rate: number,
+		maxDistance: number,
+		center: Vector3
+	) {
+		const position = rigidB?.translation(); // Get the current position
+		if (!position) return;
 
-		let distanceToCenter = Math.sqrt(x ** 2 + y ** 2 + z ** 2);
-		distanceToCenter = Math.max(distanceToCenter - orbitalDecreaseRate, 10); // Ensuring the distance doesn't go below a threshold
+		// Calculate the distance from the center
+		const distance = new Vector3(position.x, position.y, position.z).distanceTo(center);
 
-		const tangentialDirection = new Vector3(-y, x, 0).normalize();
-		const newVelocityMagnitude = Math.sqrt(distanceToCenter) * 100;
+		console.log(position);
+
+		if (distance > maxDistance) {
+			// Calculate the direction from the center to the object
+			const direction = new Vector3(position.x, position.y, position.z).sub(center).normalize();
+
+			// Set the new position at max distance along the direction
+			const newPosition = direction.multiplyScalar(maxDistance).add(center);
+			rigidB?.setTranslation(newPosition, true);
+		}
+
+		// Calculate the tangential direction
+		const tangentialDirection = new Vector3(-position.y, position.x, 0).normalize();
+		const newVelocityMagnitude = Math.sqrt(rate * distance * 200); // Adjust this calculation as needed
 		const newVelocity = tangentialDirection.multiplyScalar(newVelocityMagnitude);
 
 		rigidB?.setLinvel(newVelocity, true);
